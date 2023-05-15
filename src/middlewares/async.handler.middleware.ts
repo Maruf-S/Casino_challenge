401ae23e7a67f0b457ca6efdb4b1ab80f45d8a7f
Maruf Sherefa <56218443+Maruf-S@users.noreply.github.com>
2023-05-15 16:50:05 +0300

    Fix deployment

--- src/middlewares/async.handler.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { apiErrorHandler } from '../handlers/errorHandler';
-
-export const asyncMiddleware =
-  (fn) => (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch((error) =>
-      apiErrorHandler(error, req, res, 'An error occurred.')
-    );
